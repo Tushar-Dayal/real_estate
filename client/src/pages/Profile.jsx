6d45faeec9ff6638431a2eebe91cf1ff25a7e785
Tushar Dayal <tushardayal.1711@gmail.com>
2024-05-01 18:06:38 +0530

    add sign out function

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -18,7 +18,10 @@ import {
   updateUserFailure,
   deleteUserFailure,
   deleteUserStart,
-  deleteUserSuccess
+  deleteUserSuccess,
+  signOutUserFailure,
+  signOutUserStart,
+  signOutUserSuccess
 } from '../redux/user/userSlice';
 
 
@@ -104,6 +107,22 @@ export default function Profile() {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(data.message));
+    }
+  };
+
+
 
   useEffect(() => {
     if (file) {
@@ -177,7 +196,7 @@ export default function Profile() {
     </form>
     <div className='flex justify-between mt-5'>
     <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
-    <span className='text-red-700 cursor-pointer'>Sign Out</span>
+    <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
     </div>
 
     <p className='text-red-700 mt-5'>{error ? error : ''}</p>
