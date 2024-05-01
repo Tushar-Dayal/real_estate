91a4e78f28355457980acdb1bd1feb4793145b84
Tushar Dayal <tushardayal.1711@gmail.com>
2024-05-01 22:21:00 +0530

    complete create listing function

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,6 +2,7 @@
 import { useSelector } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import {
   getDownloadURL,
@@ -192,6 +193,14 @@ export default function Profile() {
           className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
           {loading ? 'Loading...' : 'Update'}
         </button>
+          
+        <Link
+          className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+          to={'/create-listing'}
+        >
+          Create Listing
+        </Link>
+        
       
     </form>
     <div className='flex justify-between mt-5'>
